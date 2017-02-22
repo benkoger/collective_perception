5eec5f589c3d329f7d7468b2f4e58a20d295e9aa
benkoger <benkoger@gmail.com>
2017-02-22 16:13:04 +0100

    Add files via upload

---
+++ group_perception_model.py
@@ -0,0 +1,79 @@
+import numpy as np
+import cv2
+
+class Environment:
+    def __init__(self, dims):
+        self.dims = dims
+        self.background_value = .5
+        self.visualisation = np.ones((self.dims[0], self.dims[1])) * self.background_value
+        self.features = []
+
+    def add_feature(self, feature):
+        if isinstance(feature, (list,tuple)):
+            assert not isinstance(feature, str)
+            for f in feature:
+                self.features.append(f)
+        else:
+            self.features.append(feature)
+
+
+    def draw(self, t):
+        for feature in self.features:
+            self.visualisation[feature.pos[0]:(feature.pos[0] + feature.dims[0]),
+                                feature.pos[1]:(feature.pos[1] + feature.dims[1])] = feature.make_waves(t)
+        cv2.imshow('the environment', self.visualisation)
+        cv2.waitKey(1)
+
+    def close(self):
+        #opencv on mac hack
+        for i in range(5):
+
+            cv2.destroyAllWindows()
+            cv2.waitKey(1)
+
+
+class Waves:
+    def __init__(self, pos, dims, f, theta, wave_length, direction, thresh):
+
+        self.dims = dims
+        self.x = np.linspace(0, self.dims[1], self.dims[1])
+        self.y = np.linspace(0, self.dims[0], self.dims[0])
+
+        self.x_grid, self.y_grid = np.meshgrid(self.x, self.y)
+
+        self.visualisation = np.ones((self.dims[0], self.dims[1]))
+        self.thresh = thresh
+        self.f = f
+        self.theta = theta
+        self.wave_length = wave_length
+        self.pos = pos
+        self.direction = direction # should be +/- 1
+    
+    def draw(self):
+    	cv2.imshow("decision_making", self.visualisation)
+    	cv2.waitKey(1)
+
+    def make_waves(self, t):
+
+        x_part = self.x_grid * np.cos(self.theta)
+        y_part = self.y_grid * np.sin(self.theta)
+        total_part = x_part + y_part 
+        self.visualisation = ((np.sin(np.pi*2 * (total_part / self.wave_length - 
+                                                self.direction * self.f*t)) 
+                             + 1.0) / 2.0)
+        if self.thresh == True:
+            self.visualisation = np.round(self.visualisation)
+
+        return self.visualisation
+
+    def run(self):
+        self.graphics()
+
+    def close(self):
+        #opencv on mac hack
+        for i in range(5):
+
+            cv2.destroyAllWindows()
+            cv2.waitKey(1)
+
+
